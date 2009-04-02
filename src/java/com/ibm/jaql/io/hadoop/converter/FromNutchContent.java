abdc16b03d4ba623e95c4ae10bc5a52fd4823430
vuk.ercegovac <vuk.ercegovac@553b7828-9618-11dd-899d-fb716ef6eb16>
2009-04-02 18:15:48 +0000

    added nutch converter to JSON + dependent lib.

---
+++ src/java/com/ibm/jaql/io/hadoop/converter/FromNutchContent.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright (C) IBM Corp. 2008.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.ibm.jaql.io.hadoop.converter;
+
+import org.apache.hadoop.io.Writable;
+import org.apache.hadoop.io.WritableComparable;
+import org.apache.nutch.metadata.Metadata;
+import org.apache.nutch.protocol.Content;
+
+import com.ibm.jaql.io.converter.FromItem;
+import com.ibm.jaql.json.type.Item;
+import com.ibm.jaql.json.type.JString;
+import com.ibm.jaql.json.type.MemoryJRecord;
+
+public class FromNutchContent extends HadoopRecordToItem {
+
+  public static enum Field {
+    URL("url"),
+    BASEURL("bUrl"),
+    TYPE("type"),
+    CONTENT("cont"),
+    META("meta");
+    
+    public final String name;
+    
+    Field(String n) {
+      this.name = n;
+    }
+  };
+  
+  @Override
+  protected FromItem<WritableComparable> createKeyConverter() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  // TODO: the generic should be any subclass of Writable
+  @Override
+  protected FromItem<Writable> createValConverter() {
+    return new FromItem<Writable> () {
+      public void convert(Writable src, Item tgt)
+      {
+        // expect src to be Content
+        Content c = (Content)src;
+        
+        // expect tgt's value to be MemoryJRecord 
+        MemoryJRecord r = (MemoryJRecord)tgt.get();
+        
+        // set the fixed fields
+        ((JString)r.getValue(Field.URL.name).get()).copy(c.getUrl().getBytes());
+        ((JString)r.getValue(Field.BASEURL.name).get()).copy(c.getBaseUrl().getBytes());
+        ((JString)r.getValue(Field.TYPE.name).get()).copy(c.getContentType().getBytes());
+        if(c.getContentType().indexOf("text") >= 0)
+          ((JString)r.getValue(Field.CONTENT.name).get()).copy(c.getContent());
+        else
+          ((JString)r.getValue(Field.CONTENT.name).get()).set("binary");
+        
+        // set the dynamic metadata
+        MemoryJRecord mr = (MemoryJRecord)r.getValue(Field.META.name).get();
+        mr.clear();
+        Metadata meta = c.getMetadata();
+        String[] names = meta.names();
+        for(int i = 0; i < names.length; i++) {
+          mr.add(names[i], new JString(meta.get(names[i])));
+        }
+      }
+      
+      public Item createTarget()
+      {
+        int n = Field.values().length;
+        MemoryJRecord r = new MemoryJRecord(n);
+        r.add(Field.URL.name, new JString());
+        r.add(Field.BASEURL.name, new JString());
+        r.add(Field.TYPE.name, new JString());
+        r.add(Field.CONTENT.name, new JString());
+        r.add(Field.META.name, new MemoryJRecord());
+        
+        return new Item(r);
+      }
+    };
+  }
+  
+}
