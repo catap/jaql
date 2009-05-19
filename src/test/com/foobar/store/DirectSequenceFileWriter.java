2153f1b091e71cdd9dd6d0fd161edbdc0cda00f0
vuk.ercegovac <vuk.ercegovac@553b7828-9618-11dd-899d-fb716ef6eb16>
2009-05-19 17:46:27 +0000

    example of writing directly to sequence files.

---
+++ src/test/com/foobar/store/DirectSequenceFileWriter.java
@@ -0,0 +1,96 @@
+package com.foobar.store;
+
+import java.util.Vector;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.SequenceFile;
+import org.apache.hadoop.io.SequenceFile.Writer;
+import org.apache.hadoop.mapred.SequenceFileOutputFormat;
+
+import com.ibm.jaql.io.Adapter;
+import com.ibm.jaql.io.ItemWriter;
+import com.ibm.jaql.io.OutputAdapter;
+import com.ibm.jaql.io.hadoop.DefaultHadoopOutputAdapter;
+import com.ibm.jaql.io.hadoop.FileOutputConfigurator;
+import com.ibm.jaql.io.hadoop.HadoopOutputAdapter;
+import com.ibm.jaql.json.type.Item;
+import com.ibm.jaql.json.type.JLong;
+import com.ibm.jaql.json.type.JString;
+import com.ibm.jaql.json.type.MemoryJRecord;
+
+public class DirectSequenceFileWriter {
+  
+  public void writeItemsUsingAdapter(Iterable<Item> items, String fileName) throws Exception {
+    
+    // TODO: API needs to be cleaned up to make this easier to use! In addition, this will produce
+    // a "_temporary" directory and will nest "fileName" under a directory named "fileName" making the
+    // result more difficult to use (by exposing differences between map-reduce and serial environments) 
+    
+    // setup adapter options
+    MemoryJRecord args = new MemoryJRecord();
+    MemoryJRecord options = new MemoryJRecord();
+    options.add(OutputAdapter.ADAPTER_NAME, new JString(DefaultHadoopOutputAdapter.class.getName()));
+    options.add(OutputAdapter.FORMAT_NAME, new JString(SequenceFileOutputFormat.class.getName()));
+    options.add(HadoopOutputAdapter.CONFIGURATOR_NAME, new JString(FileOutputConfigurator.class.getName()));
+    args.add(Adapter.LOCATION_NAME, new JString(fileName));
+    args.add(Adapter.OUTOPTIONS_NAME, options);
+    
+    // create adapter
+    OutputAdapter adapter = new DefaultHadoopOutputAdapter<Item, Item>();
+    adapter.initializeFrom(new Item(args));
+    
+    // open adapter
+    adapter.open();
+    
+    // get record writer
+    ItemWriter writer = adapter.getItemWriter();
+    
+    // write items
+    for(Item i : items) {
+      writer.write(i);
+    }
+    
+    // close writer
+    writer.close();
+    
+    // close adapter
+    adapter.close();
+  }
+  
+  public void writeItemsUsingSequenceFile(Iterable<Item> items, String fileName) throws Exception {
+    
+    // interrogate the environment
+    Configuration conf = new Configuration();
+    Path p = new Path(fileName);
+    FileSystem fs = p.getFileSystem(conf);
+    
+    // get the writer
+    Writer writer = SequenceFile.createWriter(fs, conf, p, Item.class, Item.class);
+    
+    // write items
+    for(Item i : items) {
+      writer.append(Item.nil, i);
+    }
+    
+    // close writer
+    writer.close();
+  }
+  
+  public static final void main(String[] args) throws Exception {
+    // sample data
+    Vector<Item> v = new Vector<Item>();
+    MemoryJRecord r = new MemoryJRecord();
+    r.add("a", new JString("sample"));
+    r.add("b", new JString("something else"));
+    v.add(new Item(r));
+    r = new MemoryJRecord();
+    r.add("a", new JLong(123));
+    r.add("c", new JString("back to string"));
+    v.add(new Item(r));
+    
+    (new DirectSequenceFileWriter()).writeItemsUsingSequenceFile(v, "fileUsingSequenceFile");
+    (new DirectSequenceFileWriter()).writeItemsUsingAdapter(v, "fileUsingAdapter");
+  }
+}
