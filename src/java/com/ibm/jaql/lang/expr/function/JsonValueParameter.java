f816ffd5bfb546df0fe13c14a9487406ce9a5ca5
yaojingguo <yaojingguo@553b7828-9618-11dd-899d-fb716ef6eb16>
2009-12-15 02:50:03 +0000

    Remove comments generated by Eclipse

--- src/java/com/ibm/jaql/lang/expr/function/JsonValueParameter.java
+++ src/java/com/ibm/jaql/lang/expr/function/JsonValueParameter.java
@@ -21,90 +21,65 @@ import com.ibm.jaql.json.type.JsonUtil;
 import com.ibm.jaql.json.type.JsonValue;
 
 /** A parameter that takes a JSON value as default value. */
-public class JsonValueParameter extends Parameter<JsonValue>
-{
-  public JsonValueParameter(JsonString name, Schema schema, boolean isRepeating)
-  {
-    super(name, schema, isRepeating);
-    // TODO Auto-generated constructor stub
-  }
+public class JsonValueParameter extends Parameter<JsonValue> {
+	public JsonValueParameter(JsonString name, Schema schema, boolean isRepeating) {
+		super(name, schema, isRepeating);
+	}
 
-  public JsonValueParameter(JsonString name, Schema schema,
-      JsonValue defaultValue)
-  {
-    super(name, schema, defaultValue);
-    // TODO Auto-generated constructor stub
-  }
+	public JsonValueParameter(JsonString name,
+	                          Schema schema,
+	                          JsonValue defaultValue) {
+		super(name, schema, defaultValue);
+	}
 
-  public JsonValueParameter(JsonString name, Schema schema)
-  {
-    super(name, schema);
-    // TODO Auto-generated constructor stub
-  }
+	public JsonValueParameter(JsonString name, Schema schema) {
+		super(name, schema);
+	}
 
-  public JsonValueParameter(JsonString name, String schema,
-      JsonValue defaultValue)
-  {
-    super(name, schema, defaultValue);
-    // TODO Auto-generated constructor stub
-  }
+	public JsonValueParameter(JsonString name,
+	                          String schema,
+	                          JsonValue defaultValue) {
+		super(name, schema, defaultValue);
+	}
 
-  public JsonValueParameter(JsonString name, String schema)
-  {
-    super(name, schema);
-    // TODO Auto-generated constructor stub
-  }
+	public JsonValueParameter(JsonString name, String schema) {
+		super(name, schema);
+	}
 
-  public JsonValueParameter(JsonString name)
-  {
-    super(name);
-    // TODO Auto-generated constructor stub
-  }
+	public JsonValueParameter(JsonString name) {
+		super(name);
+	}
 
-  public JsonValueParameter(String name, Schema schema, boolean isRepeating)
-  {
-    super(name, schema, isRepeating);
-    // TODO Auto-generated constructor stub
-  }
+	public JsonValueParameter(String name, Schema schema, boolean isRepeating) {
+		super(name, schema, isRepeating);
+	}
 
-  public JsonValueParameter(String name, Schema schema, JsonValue defaultValue)
-  {
-    super(name, schema, defaultValue);
-    // TODO Auto-generated constructor stub
-  }
+	public JsonValueParameter(String name, Schema schema, JsonValue defaultValue) {
+		super(name, schema, defaultValue);
+	}
 
-  public JsonValueParameter(String name, Schema schema)
-  {
-    super(name, schema);
-    // TODO Auto-generated constructor stub
-  }
+	public JsonValueParameter(String name, Schema schema) {
+		super(name, schema);
+	}
 
-  public JsonValueParameter(String name, String schema, JsonValue defaultValue)
-  {
-    super(name, schema, defaultValue);
-    // TODO Auto-generated constructor stub
-  }
+	public JsonValueParameter(String name, String schema, JsonValue defaultValue) {
+		super(name, schema, defaultValue);
+	}
 
-  public JsonValueParameter(String name, String schema)
-  {
-    super(name, schema);
-    // TODO Auto-generated constructor stub
-  }
+	public JsonValueParameter(String name, String schema) {
+		super(name, schema);
+	}
 
-  public JsonValueParameter(String name)
-  {
-    super(name);
-    // TODO Auto-generated constructor stub
-  }
+	public JsonValueParameter(String name) {
+		super(name);
+	}
 
-  @Override
-  protected JsonValue processDefault(JsonValue value)
-  {
-     JsonValue v = JsonUtil.getImmutableCopyUnchecked(value);
-     if (!(getSchema().matchesUnsafe(v))) 
-       throw new IllegalArgumentException("default value does not match provided schema");
-     return v;
-  }
-  
+	@Override
+	protected JsonValue processDefault(JsonValue value) {
+		JsonValue v = JsonUtil.getImmutableCopyUnchecked(value);
+		if (!(getSchema().matchesUnsafe(v)))
+			throw new IllegalArgumentException("default value does not match provided schema");
+		return v;
+	}
 
 }
