56fb787b7b7f9833625e932d182d602e0f20ea8d
Kevin Beyer <dr.beyer@gmail.com>
2010-06-05 09:15:39 +0000

    Better support for OutputCommiter that hopefully solves the problem with multiple local writes that caused test failures recently.  Also a fix for minicluster mode setting up its conf properly in the shell and during tests.

--- vendor/hadoop/0.20.1/src/jaql/test/JaqlShell.java
+++ vendor/hadoop/0.20.1/src/jaql/test/JaqlShell.java
@@ -1,12 +1,12 @@
 /*
  * Copyright (C) IBM Corp. 2008.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -14,33 +14,37 @@
  * the License.
  */
 
-import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
-
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.Iterator;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hdfs.MiniDFSCluster;
+import org.apache.hadoop.mapred.JobConf;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hbase.client.HBaseAdmin;
+import org.apache.hadoop.hbase.client.HTable;
+import org.apache.hadoop.hbase.regionserver.HRegion;
+import org.apache.hadoop.hbase.util.FSUtils;
 import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.HConstants;
+import org.apache.hadoop.hbase.HRegionInfo;
 import org.apache.hadoop.hbase.HTableDescriptor;
 import org.apache.hadoop.hbase.MiniHBaseCluster;
 import org.apache.hadoop.hbase.MiniZooKeeperCluster;
-import org.apache.hadoop.hbase.client.HBaseAdmin;
-import org.apache.hadoop.hbase.client.HTable;
-import org.apache.hadoop.hbase.util.FSUtils;
-import org.apache.hadoop.hdfs.MiniDFSCluster;
-import org.apache.hadoop.mapred.JobConf;
 import org.apache.hadoop.mapred.MiniMRCluster;
 import org.apache.hadoop.util.VersionInfo;
 
 import com.ibm.jaql.util.shell.AbstractJaqlShell;
 
 /**
- * 
+ *
  */
 public class JaqlShell extends AbstractJaqlShell
 {
@@ -55,8 +59,13 @@ public class JaqlShell extends AbstractJaqlShell
   private HBaseAdmin       m_admin;
 
   private JaqlShell() { };
-  
-  @Override
+ 
+  /**
+   * @param dir
+   * @param numNodes
+   * @param format
+   * @throws Exception
+   */
   public void init(String dir, int numNodes) throws Exception
   {
     String vInfo = VersionInfo.getVersion();
@@ -64,68 +73,71 @@ public class JaqlShell extends AbstractJaqlShell
     m_conf = new HBaseConfiguration();
 
     // setup conf according to the Hadoop version
-    if (vInfo.indexOf("0.20") >= 0)
-    {
-      m_conf.set(HConstants.CLUSTER_IS_LOCAL, "true");
-    }
-    else
+    if (vInfo.indexOf("0.20") < 0)
     {
       throw new Exception("Unsupported Hadoop version: " + vInfo);
     }
-    m_conf.set("hbase.master.info.port", "-1");
-    m_conf.set("hbase.regionserver.info.port", "-1");
-    //setupOverride();
+   
 
+    // setup the mini dfs cluster
     m_fs = new MiniDFSCluster(m_conf, numNodes, true, (String[])null);
     FileSystem filesystem = m_fs.getFileSystem();
     m_conf.set("fs.default.name", filesystem.getUri().toString());
     Path parentdir = filesystem.getHomeDirectory();
-    m_conf.set(HConstants.HBASE_DIR, parentdir.toString());
     filesystem.mkdirs(parentdir);
     FSUtils.setVersion(filesystem, parentdir);
-    
-    try
-    {
-      String hbvInfo = org.apache.hadoop.hbase.util.VersionInfo.getVersion();
-      if (hbvInfo.indexOf("0.20") >= 0)
+   
+    // setup hbase cluster (only if OS is not windows)
+    if(!System.getProperty("os.name").toLowerCase().contains("win")) {
+      m_conf.set(HConstants.HBASE_DIR, parentdir.toString());      
+      Path hdfsTestDir = filesystem.makeQualified(new Path(m_conf.get(HConstants.HBASE_DIR)));
+
+      // prime the hdfs for hbase information...
+      HRegion root = HRegion.createHRegion(HRegionInfo.ROOT_REGIONINFO, hdfsTestDir, (HBaseConfiguration)m_conf);
+      HRegion meta = HRegion.createHRegion(HRegionInfo.FIRST_META_REGIONINFO, hdfsTestDir, (HBaseConfiguration)m_conf);
+      HRegion.addRegionToMETA(root, meta);
+
+      // ... and close the root and meta
+      if (meta != null) {
+        meta.close();
+        meta.getLog().closeAndDelete();
+      }
+      if (root != null) {
+        root.close();
+        root.getLog().closeAndDelete();
+      }
+
+      try
       {
         this.zooKeeperCluster = new MiniZooKeeperCluster();
-        int clientPort = this.zooKeeperCluster.startup(new File(dir));
+        File testDir = new File(dir);
+        int clientPort = this.zooKeeperCluster.startup(testDir);
         m_conf.set("hbase.zookeeper.property.clientPort", Integer.toString(clientPort));
-
+      } catch(Exception e) {
+        LOG.error("Unable to startup zookeeper");
+        throw new IOException(e);
+      }
+      try {
         // start the mini cluster
         m_base = new MiniHBaseCluster((HBaseConfiguration)m_conf, numNodes);
-
-        // opening the META table ensures that cluster is running
-        new HTable((HBaseConfiguration)m_conf, HConstants.META_TABLE_NAME);
-        m_base = new MiniHBaseCluster((HBaseConfiguration) m_conf, numNodes);
-        //setupOverride();
+      } catch(Exception e) {
+        LOG.error("Unable to startup hbase");
+        throw new IOException(e);
       }
-    }
-    catch (Exception e)
-    {
-      LOG.warn("Could not start HBase cluster due to exception", e);
-    }
-    catch (Error er)
-    {
-      LOG.warn("Could not start HBase cluster due to error", er);
-    }
-    //m_base = new MiniHBaseCluster((HBaseConfiguration)m_conf, numNodes, true, format, false);
-    //setupOverride();
+      try {
+        // opening the META table ensures that cluster is running
+        new HTable((HBaseConfiguration)m_conf, HConstants.META_TABLE_NAME);        
 
-//    if (m_base != null)
-//    {
-//      m_mr = startMRCluster(numNodes, m_fs.getFileSystem().getUri().getAuthority(), m_conf);
-//    }
-//    else
-//    {
-//      //m_fs = new MiniDFSCluster(m_conf, numNodes, true, null);
-//      m_mr = startMRCluster(numNodes, m_fs.getFileSystem().getUri()
-//          .getAuthority(), m_conf);
-//      //m_mr = new MiniMRCluster(numNodes, m_fs.getFileSystem().getUri().getAuthority(), 1);
-//    }
+        //setupOverride(conf);
+      }
+      catch (Exception e)
+      {
+        LOG.warn("Could not verify that hbase is up", e);
+      }
+      setupOverride();
+    }
 
-    m_mr = startMRCluster(numNodes, m_fs.getFileSystem().getUri().getAuthority(), m_conf);
+    m_mr = startMRCluster(numNodes, m_fs.getFileSystem().getName(), m_conf);
 
     Configuration conf = new Configuration();
     FileSystem fs = FileSystem.get(conf);
@@ -133,29 +145,37 @@ public class JaqlShell extends AbstractJaqlShell
     // make the home directory if it does not exist
     Path hd = fs.getWorkingDirectory();
     if (!fs.exists(hd)) fs.mkdirs(hd);
-    
+   
     // make the $USER/_temporary directory if it does not exist
     Path tmpPath = new Path(hd, "_temporary");
     if (!fs.exists(tmpPath)) fs.mkdirs(tmpPath);
 
     if (m_base != null)
     {
-      m_admin = new HBaseAdmin((HBaseConfiguration) m_conf);
-      HTableDescriptor[] tables = m_admin.listTables();
-      if (tables != null)
-      {
-        for (int i = 0; i < tables.length; i++)
+      try {
+        m_admin = new HBaseAdmin((HBaseConfiguration) m_conf);
+        HTableDescriptor[] tables = m_admin.listTables();
+        if (tables != null)
         {
-          m_admin.enableTable(tables[i].getName());
+          for (int i = 0; i < tables.length; i++)
+          {
+            m_admin.enableTable(tables[i].getName());
+          }
         }
+      } catch(Exception e) {
+        LOG.warn("failed to enable hbase tables");
       }
     }
   }
 
-  @Override
+  /**
+   * @throws Exception
+   */
   public void init() throws Exception
   {
     // do nothing in the case of cluster
+    //m_conf = new HBaseConfiguration();
+    //m_admin = new HBaseAdmin(m_conf);
     Configuration conf = new Configuration();
     FileSystem fs = FileSystem.get(conf);
 
@@ -176,11 +196,12 @@ public class JaqlShell extends AbstractJaqlShell
     }
 
     // write out the JobConf from MiniMR to the override dir
-    DataOutputStream out = new DataOutputStream(new FileOutputStream(overrideDir.getCanonicalPath()
-        + File.separator + "hbase-default.xml"));
-    m_conf.write(out);
+    OutputStream out = new FileOutputStream(overrideDir.getCanonicalPath()
+        + File.separator + "hbase-default.xml");
+    m_conf.writeXml(out);
     out.close();
   }
+
 
   /**
    * @param numNodes
@@ -223,30 +244,51 @@ public class JaqlShell extends AbstractJaqlShell
       addr = jc.get(name, null);
     }
     conf.set(name, addr);
-    DataOutputStream out = new DataOutputStream(new FileOutputStream(overrideDir.getCanonicalPath()
-        + File.separator + "hadoop-default.xml"));
-    conf.write(out);
-    out.close();
+    OutputStream outCore = new FileOutputStream(overrideDir.getCanonicalPath()
+        + File.separator + "core-default.xml");
+    OutputStream outMapred = new FileOutputStream(overrideDir.getCanonicalPath()
+        + File.separator + "mapred-default.xml");
+    OutputStream outHdfs = new FileOutputStream(overrideDir.getCanonicalPath()
+        + File.separator + "hdfs-default.xml");
+    conf.writeXml(outCore);
+    conf.writeXml(outMapred);
+    conf.writeXml(outHdfs);
+    outCore.close();
+    outMapred.close();
+    outHdfs.close();
   }
 
   /**
-   * Removes the default configurations for hadoop and hbase.
-   * 
    * @throws Exception
    */
   private static void cleanupOverride() throws Exception
   {
-    String dirName = System.getProperty("hadoop.conf.override");
-    if(dirName != null) {
-      File overrideDir = new File(dirName);
-      File mrOverride = new File(overrideDir, "hadoop-default.xml");
-      File hbOverride = new File(overrideDir, "hbase-default.xml");
-      mrOverride.delete();
-      hbOverride.delete();
-    }
+    File overrideDir = new File(System.getProperty("hadoop.conf.override"));
+    File coreOverride = new File(overrideDir, "core-default.xml");
+    File mrOverride = new File(overrideDir, "mapred-default.xml");
+    File hdfsOverride = new File(overrideDir, "hdfs-default.xml");
+    File hbOverride = new File(overrideDir, "hbase-default.xml");
+    coreOverride.delete();
+    mrOverride.delete();
+    hdfsOverride.delete();
+    hbOverride.delete();
   }
 
-  @Override
+  /**
+   * @param mrc
+   * @throws Exception
+   */
+  private static void stopMRCluster(MiniMRCluster mrc) throws Exception
+  {
+    mrc.shutdown();
+
+    // clean up the temp conf dir
+    cleanupOverride();
+  }
+
+  /**
+   * @throws Exception
+   */
   public void close() throws Exception
   {
     if (m_mr != null)
@@ -270,8 +312,21 @@ public class JaqlShell extends AbstractJaqlShell
     cleanupOverride();
   }
 
-  public static void main(String[] args) throws Exception {
-  	main(new JaqlShell(), args);
+ 
+
+  /**
+   * @param args
+   * @throws Exception
+   */
+  public static void main(String[] args) throws Exception
+  {
+    try {
+      main(new JaqlShell(), args);
+    } catch(Exception e) {
+      System.exit(1);
+    }
+       
+    System.exit(0);
   }
 
 }
