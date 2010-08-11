5aee6f4cbe4a6b01eab82ec3c611e1794bbd9ca1
wwyang@cn.ibm.com <wwyang@cn.ibm.com@553b7828-9618-11dd-899d-fb716ef6eb16>
2010-08-11 07:20:30 +0000

    Update pyJaql to revision 0.2, including following changes 1. Add ability to add jars 		ext_jar=self.BASE_DIR+"/build/extension.jar" 		jaql=Jaql() 		jaql.add_jar(ext_jar) 		jaql.register_java_udf("split","com.acme.extensions.fn.Split1") 		jaql.evaluate("split('a,b,c,d,e',',');"); 		assert value==['a','b','c','d','e']
    2. Add ability to import jaql module 
    		ext_jar=self.BASE_DIR+"/build/extension.jar"
    		jaql=Jaql()
    		jaql.set_module_path(search_p)
    		jaql.evaluate("import fuzzy_join;")
    		v=jaql.evaluate("fuzzy_join::cleanTitle('XY');");
    		assert v=="XY"
    
    3. Add ability to handle jaql functions
    		jaql=Jaql()
    		jaql.register_function("sample=fn(itg,str,float,bool)([{id:itg, name:str, height:float,isStudent: bool}]->transform [$.id,$.name,$.height,$.isStudent]);")
    		infos=jaql.eval_function("sample",[12345,'jack',66.3,True]);
    		assert infos[0][0]==12345
    		assert infos[0][1]=='jack'
    		assert infos[0][2]==66.3
    		assert infos[0][3]==True
    
     Tips: API supports two ways to set function arguments, one in sequence and the other is using key-pair values(dict in python), like 
    	v=self.jaql.eval_function("samplefn",{'a':1200,'b':3200});
    
    4.Add ability to handle multiple statements
    		jaql=Jaql()
    		jaql.exectue_batch("1+1;2+2;3+3;")
    		exp_num=(2,4,6)
    		i=0
    		while self.jaql.move_next():
    			it = jaql.current()
    			for num in it:
    				assert num==exp_num[i]
    			i=i+1
    
    5. Add ability to throw illegal multiple statement exception when use evaluate()/execute() multi-statements 
    		try:
    			jaql.execute("1+1;2+2;")
    		except JaqlRuntimeException,e:
    			assert e.msg=="java.lang.IllegalArgumentException: Illegal statements, multiple statements not allowed."

--- trunk/src/python/pyJaql/pyJaql/pyJaql.py
+++ trunk/src/python/pyJaql/pyJaql/pyJaql.py
@@ -1,4 +1,4 @@
-
+# -*- coding: utf-8 -*-
 ## Copyright (C) IBM Corp. 2008.
 ##  
 ## Licensed under the Apache License, Version 2.0 (the "License"); you may not
@@ -22,13 +22,10 @@ class JVMException(Exception):
 	def __init__(self, msg):
 		self.msg=msg
 
-class JAQLRuntimeException(Exception):
+class JaqlRuntimeException(Exception):
 	def __init__(self, msg):
 		self.msg=msg
 
-class JarLoadingException(Exception):
-	def __init__(self, msg):
-		self.msg=msg
 #--------------------
 
 class Jaql:
@@ -48,118 +45,143 @@ class Jaql:
 				jpype.startJVM(jpype.getDefaultJVMPath(),"-Djava.class.path="+path)
 		except Exception,err:
 			#Know JVM limitation may fire this exception 
-			print err
-			raise JVMException("Non-specific error occurs when attemps to start a JVM")
+			raise JVMException("Non-specific error occurs when attempt to start a JVM")
 		finally:
 			#initialize jaql engine
 			pkgcom=jpype.JPackage("com")
-			Jaql=pkgcom.ibm.jaql.lang.Jaql
+			Jaql=pkgcom.ibm.jaql.lang.JaqlQuery
 			self.jaql=Jaql()
-
+		
+	"""
+	Register a jaql function
+	@argument
+		fn : function declaration
+	"""	
+	def register_function(self,fn):
+		self.jaql.setQueryString(fn)
+	
+	"""
+	Evaluate a jaql function
+	@argument
+		fn : function declaration
+	"""			
+	def eval_function(self,*args):
+		try:	
+			assert len(args)<=2
+		except Exception,err:
+			raise JaqlRuntimeException("Bad query expression : function eval_function takes 1 or 2 parameters, you input %s" % len(args))
+		FunctionArgs=jpype.JPackage("com").ibm.jaql.lang.FunctionArgs
+		funArgs=FunctionArgs()
+		try:
+			if len(args)==1:		
+				#given null arguments
+				value=self.jaql.evaluate(args[0],funArgs)
+				if value!=None:
+					if isinstance(value,jpype.JPackage("com").ibm.jaql.json.type.JsonAtom):
+						return self.__parseJsonValue(value)
+					else:
+						return json.loads(self.__parseJsonValue(value))
+			else:
+				#set args for jaql function
+				if isinstance(args[1],types.ListType) or isinstance(args[1],types.TupleType):
+					funArgs.setArguments(args[1])#in sequence
+				if isinstance(args[1],types.DictionaryType):#by key-value
+					for key in args[1]:
+						funArgs.setArgument(key,args[1][key])
+				value=self.jaql.evaluate(args[0],funArgs)
+				if value!=None:
+					if isinstance(value,jpype.JPackage("com").ibm.jaql.json.type.JsonAtom):
+						return self.__parseJsonValue(value)
+					else:
+						return json.loads(self.__parseJsonValue(value))
+		except Exception,err:
+			raise JaqlRuntimeException(err)
+		
+	def exec_function(self,*args):
+		try:	
+			assert len(args)<=2
+		except Exception,err:
+			raise JaqlRuntimeException("Bad query expression : function eval_function takes 1 or 2 parameters, you input %s" % len(args))
+		FunctionArgs=jpype.JPackage("com").ibm.jaql.lang.FunctionArgs
+		funArgs=FunctionArgs()
+		try:
+			if len(args)==1:		
+				#given null arguments
+				it=self.jaql.iterate(args[0],funArgs)
+				while it.moveNext():
+					value=it.current()
+					if isinstance(value,jpype.JPackage("com").ibm.jaql.json.type.JsonAtom):
+						yield self.__parseJsonValue(value)
+					else:
+						yield json.loads(self.__parseJsonValue(value))
+			else:
+				#set args for jaql function
+				if isinstance(args[1],types.ListType) or isinstance(args[1],types.TupleType):
+					funArgs.setArguments(args[1])#in sequence
+				if isinstance(args[1],types.DictionaryType):#by key-value
+					for key in args[1]:
+						funArgs.setArgument(key,args[1][key])
+				it=self.jaql.iterate(args[0],funArgs)
+				while it.moveNext():
+					value=it.current()
+					if isinstance(value,jpype.JPackage("com").ibm.jaql.json.type.JsonAtom):
+						yield self.__parseJsonValue(value)
+					else:
+						yield json.loads(self.__parseJsonValue(value))
+		except Exception,err:
+			raise JaqlRuntimeException(err)		
+	
+	"""
+	register a batch script, prepare to execute
+	"""	
+	def exectue_batch(self, *args):
+		try:
+			self.jaql.setQueryString(args[0])
+			self.__setVars(args)
+		except Exception,err:
+			raise JaqlRuntimeException(str(err))
+		
+	"""
+	move to next cursor, if it's evaluate-able statement, evaluate it and set the current result
+	"""
+	def move_next(self):
+		return self.jaql.moveNextQuery()
+	
+	"""get current result"""
+	def current(self):
+		it=self.jaql.currentQuery()
+		while it.moveNext():
+			jv=it.current()
+			if isinstance(jv,jpype.JPackage("com").ibm.jaql.json.type.JsonAtom):
+				    #if type is JsonAtom, convert to python	types and return	
+					yield self.__parseJsonValue(jv)
+			else: 	
+				#if type is JsonArray or JsonRecord, use json module to convert them to python types						
+				#JsonArray -> list
+				#JsonRecord -> dict
+				yield json.loads(self.__parseJsonValue(jv))
+	
 	"""
 	Execute a jaql expression and return a generator
 	@Arguments: args is a list to take mutable arguments. 
 	@ #1 jaql query expression  |  <str>  | [required]
-        @ #2 jaql query variable set  | <dict> | [optional]
+    @ #2 jaql query variable set  | <dict> | [optional]
 	@return : result set | generator
 	"""
 	def execute(self,*args):
 		#1 jaqlExp = "expression only"
 		#2 jaqlExp = "expression" % dict
 		#3 jaqlExp = "expression" % {..dict data..}
-		try:		
-			assert len(args)<=2
-		except Exception,err:
-			print "Bad query expression : function execute takes 1 or 2 parameters, you input %s" % len(args)
-
-		self.jaql.setInput(args[0])		
-
-		if len(args)==1:		
-			#expression only, do nothing, prepared to execute
-			pass
-		else:
-			expr=args[0]
-			params=args[1]
-			#expression with parameter extension, setVars to the expression
-			for key in iter(params):
-				varkey=key.rjust(len(key)+1,'$')#adjust key format, comply with "$key" format
-				self.__setVar(varkey,params[key])#set variables
-		
 		#execute jaql query
 		try:
-			it=self.jaql.iter()
-			while it.moveNext():
-				jv=it.current()
-				#if type is JsonAtom, convert to python
-				if isinstance(jv,jpype.JPackage("com").ibm.jaql.json.type.JsonAtom):
-					jv_str=jv.toString()				
-					if isinstance(jv,jpype.JPackage("com").ibm.jaql.json.type.JsonString):
-						#conver JsonString to str
-						#json_value=jv_str.center(len(str(jv_str))+2,'"')# add " surround the str, make it to be valid JSON format	print "****************** is jsonstring *************************"
-						yield str(jv_str)
-					else:
-						#eval expr and return right format data				
-						yield json.loads(str(jv_str))
-					
-				else: 	
-					#if type is JsonArray or JsonRecord, use json module to convert them to python types						
-					#JsonArray -> list
-					#JsonRecord -> dict
-					yield json.loads(str(it.current().toString()))
+			self.jaql.setQueryString(args[0])
+			self.__setVars(args)
+			it=self.__iterate()
+			for item in it:
+				yield item
 		except Exception,err:
-			raise JAQLRuntimeException(str(err))
+			raise JaqlRuntimeException(str(err))
 
-	"""
-	Execute jaql expression which is defined in a file.
-	@Arguments: args is a list to take mutable arguments. 
-	@ #1 jaql query expression  |  <str>  | [required]
-        @ #2 jaql script file path  | <str> | [optional]
-	@return : result set | generator
-	"""
-	def execute_script(self,*args):
-	
-		try:		
-			assert len(args)<=2
-		except Exception,err:
-			print "Bad query expression : function execute takes 1 or 2 parameters, you input %s" % len(args)
-		# extract script file, prepared to execute	
-		JFile=jpype.JPackage("java").io.File
-		f=JFile(args[0])
-		self.jaql.setInput(args[0],jpype.JPackage("java").io.FileReader(f))
-		if len(args)==1:
-			#do nothing
-			pass
-		else:
-			expr=args[0]
-			params=args[1]
-			#expression with parameter extension, setVars to the expression
-			for key in iter(params):
-				varkey=key.rjust(len(key)+1,'$')#adjust key format, comply with "$key" format
-				self.__setVar(varkey,params[key])#set variables
-		
-		#execute jaql query
-		try:
-			it=self.jaql.iter()
-			while it.moveNext():
-				jv=it.current()
-				#if type is JsonAtom, convert to python
-				if isinstance(jv,jpype.JPackage("com").ibm.jaql.json.type.JsonAtom):
-					jv_str=jv.toString()
-					if isinstance(jv,jpype.JPackage("com").ibm.jaql.json.type.JsonString):
-						#conver JsonString to str
-						#json_value=jv_str.center(len(str(jv_str))+2,'"')# add " surround the str, make it to be valid JSON format
-						yield str(jv_str)
-					else:
-						#eval expr and return right format data					
-						yield json.loads(str(jv_str))
-				else: 	
-					#if type is JsonArray or JsonRecord, use json module to convert them to python types						
-					#JsonArray -> list
-					#JsonRecord -> dict
-					print it.current()
-					yield json.loads(str(it.current().toString()))
-		except Exception,err:
-			raise JAQLRuntimeException(str(err))
 
 	"""
 	Evaluate jaql expression
@@ -169,88 +191,20 @@ class Jaql:
 	@return : result set | <list>
 	"""
 	def evaluate(self,*args):
-		try:		
-			assert len(args)<=2
-		except Exception,err:
-			print "Bad query expression : function execute takes 1 or 2 parameters, you input %s" % len(args)
-		self.jaql.setInput(args[0])		
-		if len(args)==1:		
-			#expression only, do nothing, prepared to execute
-			pass
-		else:
-			expr=args[0]
-			params=args[1]
-			#expression with parameter extension, setVars to the expression
-			for key in iter(params):
-				varkey=key.rjust(len(key)+1,'$')#adjust key format, comply with "$key" format
-				self.__setVar(varkey,params[key])#set variables
 		try:
-			value=self.jaql.evalNext()
-			return json.loads(str(value.toString()))
+			self.jaql.setQueryString(args[0])
+			self.__setVars(args)
+			return self.__evaluate()
 		except Exception,err:
-			raise JAQLRuntimeException(str(err))
+			raise JaqlRuntimeException(str(err))
 
-	"""
-	Evaluate jaql script
-	@Arguments: args is a list to take mutable arguments. 
-	@ #1 jaql query expression  |  <str>  | [required]
-    @ #2 jaql query variable set  | <dict> | [optional]
-	@return : result set | <list>
-	"""
-	def evaluate_script(self,*args):
-		try:		
-			assert len(args)<=2
-		except Exception,err:
-			print "Bad query expression : function execute takes 1 or 2 parameters, you input %s" % len(args)
-		# extract script file, prepared to execute	
-		JFile=jpype.JPackage("java").io.File
-		f=JFile(args[0])
-		self.jaql.setInput(args[0],jpype.JPackage("java").io.FileReader(f))
-		if len(args)==1:
-			#do nothing
-			pass
-		else:
-			expr=args[0]
-			params=args[1]
-			#expression with parameter extension, setVars to the expression
-			for key in iter(params):
-				varkey=key.rjust(len(key)+1,'$')#adjust key format, comply with "$key" format
-				self.__setVar(varkey,params[key])#set variables
+	
+	def execute_script(self,filepath):
 		try:
-			value=self.jaql.evalNext()
-			return json.loads(str(value.toString()))
+			self.jaql.exectueBatch(filepath)
 		except Exception,err:
-			raise JAQLRuntimeException(str(err))
-	
+			raise JaqlRuntimeException(err)	
 
-	"""
-	private function
-	Set values to variable, when use setVar() in jaql.java, we did type convertion to satisfy it's argument spec
-		#|    python   |    jaql      |
-		#|-------------|--------------|	
-		#| StringType  |  JsonString  | 
-		#| IntType     |  JsonDecimal | 
-		#| FloatType   |  JsonDouble  |
-		#| LongType    |  JsonLong    |
-		#| BooleanType |  JsonBool    |
-	"""
-	def __setVar(self,varName,value):	
-		#Refer to jaql package com.ibm.jaql.json.type		
-		pkg=jpype.JPackage("com").ibm.jaql.json.type
-		if type(value)==types.StringType:
-			self.jaql.setVar(varName,pkg.JsonString(value))
-		if type(value)==types.IntType:
-			self.jaql.setVar(varName,pkg.JsonDecimal(value))
-		if type(value)==types.FloatType:
-			self.jaql.setVar(varName,pkg.JsonDouble(value))
-		if type(value)==types.LongType:
-			self.jaql.setVar(varName,pkg.JsonLong(value))
-		if type(value)==types.BooleanType:	
-			if value :
-				self.jaql.setVar(varName,pkg.MutableJsonBool(True))	
-			else:	
-				self.jaql.setVar(varName,pkg.MutableJsonBool(False))
-	
 	"""
 	Add extension jars, enable 3rd party user definded functions.
 	@Argument: path - jar file path
@@ -258,34 +212,140 @@ class Jaql:
 	def add_jar(self,path):
 		try:		
 			self.jaql.addJar(path)
-		except JarLoadingException,err:
-			raise JarLoadingException("jar not found")
+		except Exception,err:
+			raise JaqlRuntimeException("jar not found")
+		
+	"""
+	Set module search path
+	@Argument : 
+		module path
+	"""
+	def set_module_path(self,*args):
+		try:
+			JString=jpype.JPackage("java").lang.String
+			strs=JString(args[0])
+			self.jaql.setModuleSeachPath(strs)
+		except Exception,err:
+			raise JaqlRuntimeException(err)
+		
+	"""
+	register a java udf
+	@Arguments
+	@#1 udfname
+		java udf name
+	@#2 udfpath
+		java udf class path
+	"""
+	def register_java_udf(self,udfname,udfpath):
+		try:
+			self.jaql.registerJavaUDF(udfname,udfpath)
+		except Exception,err:
+			raise JaqlRuntimeException(err)
+	
+	"""
+	Evaluate a jaql query
+	"""
+	def __evaluate(self):
+		value=self.jaql.evaluate()
+		if value!=None:
+			if isinstance(value,jpype.JPackage("com").ibm.jaql.json.type.JsonAtom):
+				return self.__parseJsonValue(value)
+			else:
+				return json.loads(self.__parseJsonValue(value))
+	
+	"""
+	Execute a jaql query
+	"""
+	def __iterate(self):
+		it=self.jaql.iterate()
+		while it.moveNext():
+			jv=it.current()
+			if isinstance(jv,jpype.JPackage("com").ibm.jaql.json.type.JsonAtom):
+				yield self.__parseJsonValue(jv)
+			else:
+				yield json.loads(self.__parseJsonValue(jv))  
+				
+	"""
+	Set arguments for jaql query
+	"""
+	def __setVars(self,args):
+		try:	
+			assert len(args)<=2
+		except Exception,err:
+			raise JaqlRuntimeException("Bad query expression : function execute takes 1 or 2 parameters, you input %s" % len(args))
+		if len(args)==1:		
+			#expression only, do nothing, prepared to execute
+			pass
+		else:
+			if isinstance(args[1],types.DictionaryType): 
+				params=args[1]
+				#expression with parameter extension, setVars to the expression
+				for key in iter(params):
+					#varkey=key.rjust(len(key)+1,'$')#adjust key format, comply with "$key" format
+					self.__setVar(key,params[key])#set variables 
+			else:
+			     raise JaqlRuntimeException("Argument type %s is not usable" % type(args[1]))
+	
+	"""
+    Set values to variable, when use setVar() in jaql.java, we did type convertion to satisfy it's argument spec
+    #|    python   |    jaql      |
+    #|-------------|--------------| 
+    #| StringType  |  JsonString  | 
+    #| IntType     |  JsonDecimal | 
+    #| FloatType   |  JsonDouble  |
+    #| LongType    |  JsonLong    |
+    #| BooleanType |  JsonBool    |
+    """	    
+	def __setVar(self,varName,value):
+		pkg=jpype.JPackage("com").ibm.jaql.json.type
+		if type(value)==types.StringType:
+			self.jaql.setVar(varName,pkg.JsonString(value))
+		if type(value)==types.IntType:
+			self.jaql.setVar(varName,pkg.JsonDecimal(value))
+		if type(value)==types.FloatType:   
+			self.jaql.setVar(varName,pkg.JsonDouble(value))
+		if type(value)==types.LongType:
+			self.jaql.setVar(varName,pkg.JsonLong(value))
+		if type(value)==types.BooleanType:
+			if value : self.jaql.setVar(varName,pkg.MutableJsonBool(True))
+			else : self.jaql.setVar(varName,pkg.MutableJsonBool(False))
+
 
 	"""
-	Set property for jaql engine, jaql supports 3 runtime properties, they are "enableRewrite" (default true),"stopOnException" (default true) and "JaqlPrinter"
-	@Arguments:
-        @ #1 the name of the propery. VALID NAMES: "enableRewrite", "stopOnException", "JaqlPrinter"
-	@ #2 the value of the property are str
-	@    if property name is "enableRewrite" , value should be "true" or "false" 
-	@    if property name is "stopOnException", value should be "true" or "false" 
-        @    if property name is "JaqlPrinter", value should be valid printer class name
+	parse json value to native python types
+	if is atom json value, parse to python basic type
+	if is jsonarrya or jsonrecord, return its equivalent str value
+	if is null, just return
 	"""
-	def set_property(self,name,value):
-		self.jaql.setProperty(name,value)		
-
+	def __parseJsonValue(self, json):
+		pkg=jpype.JPackage("com").ibm.jaql.json.type
+		if isinstance(json,pkg.JsonString):
+			return str(json.toString())
+		if isinstance(json,pkg.JsonDecimal):
+			return json.intValue()
+		if isinstance(json,pkg.JsonDouble):
+			return json.doubleValue()
+		if isinstance(json,pkg.JsonLong):
+			return json.longValue()
+		if isinstance(json,pkg.JsonBool):
+			if json.get()==1 : return True
+			else : return False
+		if isinstance(json,pkg.JsonRecord):
+			return json.toString()
+		if isinstance(json,pkg.JsonArray):
+			return json.toString()
+		else : return json
+		
 	"""
-	Get property value by name
-	@Argument: property name
-	@Return: property value <str>
-	"""
-	def get_property(self,name):
-		return str(self.jaql.getProperty(name))
-
+	return true if jvm is already started
+	"""	
 	def isJVMStarted(self):
 		return jpype.isJVMStarted()
+	
 	"""
 	Close JAQL engine
 	"""
 	def close(self):
 		if jpype.isJVMStarted():
 			jpype.shutdownJVM()
+			
