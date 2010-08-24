8be0ce8d8dce85b49f6d938383ff1b5e26b4b260
Kevin.Beyer.Jaql <Kevin.Beyer.Jaql@553b7828-9618-11dd-899d-fb716ef6eb16>
2010-08-24 22:47:12 +0000

    got minicluster working.  fixed CompositeOutputAdapter bug in cluster mode

--- bin/start-minicluster.sh
+++ bin/start-minicluster.sh
@@ -11,20 +11,35 @@ if [ "$MINICLUSTER_HOME" = "" ]; then
 	export MINICLUSTER_HOME=${JAQL_HOME}/minicluster
 fi
 
+echo "starting minicluster in ${MINICLUSTER_HOME}"
+
+export HADOOP_OPTS="$HADOOP_OPTS -Dhadoop.minicluster.dir=${MINICLUSTER_HOME}"
 export HADOOP_CONF_OVERRIDE_DIR="${MINICLUSTER_HOME}/conf"
 export HADOOP_LOG_DIR="${MINICLUSTER_HOME}/logs"
 outFile="${MINICLUSTER_HOME}/minicluster.out"
 pidFile="${MINICLUSTER_HOME}/minicluster.pid"
 
+rm -fr ${HADOOP_LOG_DIR}/history
 rm -f ${HADOOP_LOG_DIR}/*
 
-echo "starting minicluster..."
-nohup ${JAQL_HOME}/bin/jaql com.ibm.jaql.MiniCluster "-Dhadoop.minicluster.dir=${MINICLUSTER_HOME}" "$@" > "$outFile" 2>&1 < /dev/null &
-# ${JAQL_HOME}/bin/jaql com.ibm.jaql.MiniCluster "-Dhadoop.minicluster.dir=${MINICLUSTER_HOME}" "$@" 
+echo "launching minicluster..."
+${JAQL_HOME}/bin/jaql com.ibm.jaql.MiniCluster "$@" > "$outFile" 2>&1 < /dev/null &
 pid=$!
 
 echo $pid > $pidFile
 echo "started minicluster under process $pid"
 
 sleep 3
-tail ${HADOOP_LOG_DIR}/minicluster.out
+tail ${outFile}
+
+export HADOOP_CONF_DIR=${HADOOP_CONF_OVERRIDE_DIR}
+export HADOOP_CONF_OVERRIDE_DIR=
+export PS1="minicluster> "
+
+echo "Running shell configured for the minicluster."
+echo "Type exit to terminate minicluster."
+$SHELL -i
+
+. ${JAQL_HOME}/bin/stop-minicluster.sh
+
+
