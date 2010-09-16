cab02e81fe38991b94afd5f1170041ecb803222a
Sarah.Libing@gmail.com <Sarah.Libing@gmail.com@553b7828-9618-11dd-899d-fb716ef6eb16>
2010-09-16 03:09:55 +0000

    example for unit testing

---
+++ trunk/home/libing/workspace/jaql-opensource/src/test/Echo.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) IBM Corp. 2010.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+//package com.acme.extensions.fn;
+
+public class Echo {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) throws Exception{
+        // TODO Auto-generated method stub
+        java.io.BufferedReader br = new java.io.BufferedReader(
+                new java.io.InputStreamReader(System.in));
+        String s = br.readLine();
+        while(s != null && !s.equalsIgnoreCase("off")){  
+            System.out.println(s);
+            s = br.readLine();
+        }
+    }
+
+}
